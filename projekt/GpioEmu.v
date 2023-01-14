849cbb33bf64d69e5e7a415f11c230a49e4ea48c
atlomak <andrzej.tlomak01@gmail.com>
2023-01-14 16:45:39 +0100

    Regs refactor in logic

--- projekt/GpioEmu.v
+++ projekt/GpioEmu.v
@@ -36,37 +36,37 @@ reg [31:0] a,b;
 
 always@(posedge clk, posedge reset)
 	if(reset) begin
-		working <= 1'b0;
-		done <= 1'b0;
+		S[3] <= 1'b0;
 		a <= 32'b0;
 		b <= 32'b0;
-		result <= 32'b0;
+		W <= 32'b0;
 	end
 	else begin
-		done <= 1'b0;
 		if(clk_en) begin
-			if(!working) begin
+			if(!S[3]) begin
 				if(start) begin
-					working <= 1'b1;
+					S[3] <= 1'b1;
 					a <= A1;
 					b <= A2;
 				end
 				else begin
-					working <= 1'b0;
+					S[3] <= 1'b0;
 				end
 			end
 			else
 				if(a != b) begin
-					working <= 1'b1;
+					S[3] <= 1'b1;
 					if(a < b)
 						b <= b - a;
 					else	
 						a <= a - b;
 				end
 				else begin
-					result <= a;
-					working <= 1'b0;
-					done <= 1'b1;
+					W <= a;
+					S[3] <= 1'b0;
+				end
+		end
+	end
 				end
 		end
 	end
