1dfa6b519346bf942140814e5d7fd36c3cc8ae3a
atlomak <andrzej.tlomak01@gmail.com>
2023-01-14 15:57:36 +0100

    Added interface for module

--- projekt/GpioEmu.v
+++ projekt/GpioEmu.v
@@ -1,14 +1,37 @@
-module gcd
-(
-		input reset,
-		input clk,
-		input clk_en,
-		input start,
-		input [31:0] dataa, datab,
-		output reg done,
-		output reg [31:0] result
-);
+module gpioemu(
+	n_reset, 							//magistrala z CPU
+	saddress[15:0], srd, swr,
+	sdata_in[31:0], sdata_out[31:0],
+	gpio_in[31:0], gpio_latch, 			//styk z GPIO - in
+	gpio_out[31:0], 					//styk z GPIO = out
+	clk, 								//sygnał opcjonalny - zegar 1KHz
+	gpio_in_s_insp[31:0]);
 
+// Interface
+
+input reset;
+input srd;
+input swr;
+input gpio_latch;
+input clk;
+
+input [15:0] saddress;
+input [31:0] sdata_in;
+input [31:0] gpio_in;
+
+output [31:0] sdata_out;
+output [31:0] gpio_out;
+output [31:0] gpio_in_s_insp;
+
+// GCD REGs
+
+reg clk_en;
+reg start;
+reg [31:0] A1;
+reg [31:0] A2,
+reg done;
+
+reg [31:0] result
 reg [31:0] a,b;
 reg working;
 
@@ -26,8 +49,8 @@ always@(posedge clk, posedge reset)
 			if(!working) begin
 				if(start) begin
 					working <= 1'b1;
-					a <= dataa;
-					b <= datab;
+					a <= A1;
+					b <= A2;
 				end
 				else begin
 					working <= 1'b0;
