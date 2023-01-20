1858427db984d97dc814554f2f1dfd6fe5ef9c04
atlomak <01159465@pw.edu.pl>
2023-01-20 02:51:40 +0100

    added build script

---
+++ projekt/build_project.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -f "qemu-system-riscv32-sykt" ]; then
+    makeQemuGpioEmu ./verilog_module/gpioemu.v && \
+    make_busybox_kernel_module 
+    ./qemu-system-riscv32-sykt -M sykt -nographic \
+    -bios fw_jump.elf \
+    -kernel Image \
+    -append "root=/dev/vda ro" \
+    -drive file=rootfs.ext2,format=raw,id=hd0 \
+    -device virtio-blk-device,drive=hd0 \
+    -netdev user,id=net0 -device virtio-net-device,netdev=net0
+else
+    make_busybox_kernel_module
+    echo "qemu-system-riscv32-sykt already exists, skipping makeQemuGpioEmu"
+    ./qemu-system-riscv32-sykt -M sykt -nographic \
+    -bios fw_jump.elf \
+    -kernel Image \
+    -append "root=/dev/vda ro" \
+    -drive file=rootfs.ext2,format=raw,id=hd0 \
+    -device virtio-blk-device,drive=hd0 \
+    -netdev user,id=net0 -device virtio-net-device,netdev=net0
+fi
